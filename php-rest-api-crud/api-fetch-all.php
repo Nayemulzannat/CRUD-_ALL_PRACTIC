686e7dec49f2680819cf1a5890869fd33bdb881c
MD Nayem <nayemtp@gmail.com>
2023-09-01 16:57:01 +0600

    rest api crudn

--- php-rest-api-crud/api-fetch-all.php
+++ php-rest-api-crud/api-fetch-all.php
@@ -1,4 +1,13 @@
 <?php
+header('content-type: application/json');
+header('Acess-Control-Allow-origin:*');
 
-
-?>
+include "config.php";
+$sql = "select * from students";
+$result = mysqli_query($conn, $sql) or die("Not Query Result");
+if (mysqli_num_rows($result) > 0) {
+    $output = mysqli_fetch_row($result);
+    echo json_encode($output);
+} else {
+    echo json_encode(array('massage' => 'No Record Found.', 'status => false'));
+}
--- php-rest-api-crud/api-fetch-single.php
+++ php-rest-api-crud/api-fetch-single.php
@@ -1,5 +1,4 @@
 <?php
-include "config.php";
-$sql= "select * from students";
-$result= mysqli_query($conn , $sql) or die("Not Query Result");
+
+
 ?>
