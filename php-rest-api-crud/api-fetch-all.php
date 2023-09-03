42ca20dee2096e31b8ae1c4abeaa30d63c2d3b00
MD Nayem <nayemtp@gmail.com>
2023-09-03 19:59:36 +0600

    API read practic

--- php-rest-api-crud/api-fetch-all.php
+++ php-rest-api-crud/api-fetch-all.php
@@ -6,8 +6,9 @@ include "config.php";
 $sql = "select * from students";
 $result = mysqli_query($conn, $sql) or die("Not Query Result");
 if (mysqli_num_rows($result) > 0) {
-    $output = mysqli_fetch_row($result);
+    $output = mysqli_fetch_all($result , MYSQLI_ASSOC);
     echo json_encode($output);
 } else {
     echo json_encode(array('massage' => 'No Record Found.', 'status => false'));
 }
+?>
--- php-rest-api-crud/api-fetch-single.php
+++ php-rest-api-crud/api-fetch-single.php
@@ -1,4 +1,17 @@
 <?php
+header('content-type: application/json');
+header('Acess-Control-Allow-origin:*');
 
+$data = json_decode(file_get_contents("php://input"), true);
+$student_id = $data['roll'];
 
+include "config.php";
+$sql = "select * from students where roll = {$student_id}";
+$result = mysqli_query($conn, $sql) or die("Not Query Result");
+if (mysqli_num_rows($result) > 0) {
+    $output = mysqli_fetch_all($result , MYSQLI_ASSOC);
+    echo json_encode($output);
+} else {
+    echo json_encode(array('massage' => 'No Record Found.', 'status => false'));
+}
 ?>
