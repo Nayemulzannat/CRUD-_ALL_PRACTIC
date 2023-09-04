fce17ee19aee5c19b2ef4ea507408c6ec595f6e8
MD Nayem <nayemtp@gmail.com>
2023-09-04 20:14:19 +0600

    API

--- php-rest-api-crud/api-insert.php
+++ php-rest-api-crud/api-insert.php
@@ -1,5 +1,25 @@
 <?php
+// header('content-type: application/json');
+// header('Acess-Control-Allow-origin:*');
+// header('Access-Control-Allow-Methods: POST');
+// header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Rrquested-With');
 
 
 
-?>
+$data = json_decode(file_get_contents("php://input"), true);
+
+$roll = $data['roll'];
+$name = $data['name'];
+$age = $data['age'];
+$gender = $data['gender'];
+
+
+include "config.php";
+$sql = "INSERT INTO students(roll,name,age,gender) VALUES ({$roll},'{$name}',{$age},'{$gender}')";
+
+$result = mysqli_query($conn, $sql) or die("Not Query Result");
+if ($result) {
+    echo json_encode(array('massage' => 'Record Inserted.', 'status => true'));
+}else {
+    echo json_encode(array('massage' => ' Record Found.', 'status => false'));
+}
