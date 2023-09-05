92d3d6a0f64bc004d20b6c66a2271fa6fcfa5776
MD Nayem <nayemtp@gmail.com>
2023-09-05 20:03:13 +0600

    API

--- php-rest-api-crud/api-update.php
+++ php-rest-api-crud/api-update.php
@@ -1,5 +1,26 @@
 <?php
+// header('content-type: application/json');
+// header('Acess-Control-Allow-origin:*');
+// header('Access-Control-Allow-Methods: POST');
+// header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Rrquested-With');
 
 
 
-?>
+$data = json_decode(file_get_contents("php://input"), true);
+
+$roll = $data['roll'];
+$name = $data['name'];
+$age = $data['age'];
+$gender = $data['gender'];
+
+
+include "config.php";
+$sql = "UPDATE students SET roll ={$roll}, name ='{$name}', age = {$age}, gender = {$gender} WHERE roll = {$roll}";
+
+$result = mysqli_query($conn, $sql) or die("Not Query Result");
+if ($result) {
+    echo json_encode(array('massage' => 'Record Update.', 'status => true'));
+}else {
+    echo json_encode(array('massage' => ' Record Not Found.', 'status => false'));
+}
+
